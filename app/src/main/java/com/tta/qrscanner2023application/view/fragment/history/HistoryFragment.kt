707b0409c22df99b8cb51627986a0a056eccca0b
tran the anh <theanh682001@gmail.com>
2024-04-17 16:10:20 +0700

    create fragment_history

--- app/src/main/java/com/tta/qrscanner2023application/view/fragment/history/HistoryFragment.kt
+++ app/src/main/java/com/tta/qrscanner2023application/view/fragment/history/HistoryFragment.kt
@@ -1,11 +1,52 @@
 package com.tta.qrscanner2023application.view.fragment.history
 
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 import com.tta.fitnessapplication.view.base.BaseFragment
 import com.tta.qrscanner2023application.databinding.FragmentHistoryBinding
 
 class HistoryFragment : BaseFragment<FragmentHistoryBinding>() {
     override var isTerminalBackKeyActive: Boolean = false
+    private lateinit var adapterPager: FragmentPageAdapter
     override fun getDataBinding(): FragmentHistoryBinding {
         return FragmentHistoryBinding.inflate(layoutInflater)
+    }
+
+    override fun initView() {
+        super.initView()
+        initUiPager()
+    }
+
+    private fun initUiPager()  = with(binding){
+        adapterPager = FragmentPageAdapter(requireActivity().supportFragmentManager, lifecycle)
+
+        tabLayout.addTab(tabLayout.newTab().setText("Scan"))
+        tabLayout.addTab(tabLayout.newTab().setText("Created"))
+
+        viewPager2.adapter = adapterPager
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab != null) {
+                    viewPager2.currentItem = tab.position
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+        })
+
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+        })
     }
 }
--- app/src/main/java/com/tta/qrscanner2023application/view/fragment/history/ListHistoryFragment.kt
+++ app/src/main/java/com/tta/qrscanner2023application/view/fragment/history/ListHistoryFragment.kt
@@ -1,11 +1,23 @@
 package com.tta.qrscanner2023application.view.fragment.history
 
+import android.widget.Toast
 import com.tta.fitnessapplication.view.base.BaseFragment
 import com.tta.qrscanner2023application.databinding.FragmentListHistoryBinding
 
 class ListHistoryFragment (type : Int) : BaseFragment<FragmentListHistoryBinding>() {
+    private val type = type
     override var isTerminalBackKeyActive: Boolean = false
     override fun getDataBinding(): FragmentListHistoryBinding {
         return FragmentListHistoryBinding.inflate(layoutInflater)
+    }
+
+    override fun initView() {
+        super.initView()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(requireContext(), type.toString(), Toast.LENGTH_LONG).show()
     }
 }
